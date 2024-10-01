6051867be55a9c1b0b0248051e6b9132826fafaa
thebruindev <yoanmaudon@gmail.com>
2024-10-01 19:31:26 +0200

    added full crud for goals need to refactor code for using convex better

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,30 @@
-"use client"
+"use client";
 
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { CreateProjectButton } from "./projects/create-project-form";
 import { ProjectCard } from "./projects/projectCard";
+import Image from "next/image";
 
 export default function Home() {
   const projects = useQuery(api.projects.getProjects);
   return (
     <main className="p-24 space-y-8">
       <div className="flex justify-between items-center">
-        <h1 className="ml-2 text-4xl font-bold text-[#005A4E] dark:text-[#DBF9FF] border-b-4 border-[#57E1C0]">My Projects</h1>
-        <CreateProjectButton/>
+        <h1 className="ml-2 text-4xl font-bold text-[#005A4E] dark:text-[#DBF9FF] border-b-4 border-[#57E1C0]">
+          My Projects
+        </h1>
+        <CreateProjectButton />
       </div>
 
       <div className="grid grid-cols-4 gap-8">
-        {projects?.map((project) => <ProjectCard key={project._id} project={project} />)}
+        {projects && projects.length < 1 ? (
+          <Image alt="plant" src="/plant.svg" width={40} height={40} />
+        ) : (
+          projects?.map((project) => (
+            <ProjectCard key={project._id} project={project} />
+          ))
+        )}
       </div>
     </main>
   );
--- convex/goals.ts
+++ convex/goals.ts
@@ -53,20 +53,31 @@ export const createGoal = mutation({
   },
 });
 
-//TODO: updating with convex
+export const updateGoalById = mutation({
+  args: { goalId: v.id("goals"), goalObjectToUpdate: goalObjectToUpdate },
+  async handler(ctx, args) {
+    const { goalObjectToUpdate } = args;
 
-// export const updateGoalById = mutation({
-//   args: goalObjectToUpdate,
-//   async handler(ctx, args) {
-//     return await ctx.db.patch("goals", {
-//       title: args.title,
-//       description: args.description,
-//       status: args.status,
-//       priority: args.priority,
-//       startDate: args.startDate,
-//       dueDate: args.dueDate,
-//       completedAt: args.completedAt,
-//       lastUpdatedAt: args.lastUpdatedAt,
-//     });
-//   },
+    // Dynamically filter out undefined values
+    const updateData = Object.fromEntries(
+      Object.entries(goalObjectToUpdate).filter(
+        ([, value]) => value !== undefined
+      )
+    );
+
+    // Patch the document with the filtered data
+    return await ctx.db.patch(args.goalId, updateData);
+  },
+});
+
+export const deleteGoalById = mutation({
+  args: { id: v.id("goals") },
+  handler: async (ctx, args) => {
+    await ctx.db.delete(args.id);
+  },
+});
+
+// export const deleteGoalById = internalMutation({
+//   args: { id: v.id("goals") },
+//   handler: (ctx, args) => ctx.db.delete(args.id),
 // });
