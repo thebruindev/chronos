7f11512be628cb56f9a7a5231251320ab06617da
thebruindev <yoanmaudon@gmail.com>
2024-10-03 17:15:12 +0200

    ready for push and mr

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,59 +1,27 @@
 "use client";
 
-import { CreateGoalButton } from "@/app/goals/create-goal-form";
-import { GoalCard } from "@/app/goals/goalCard";
-import Stack from "@/components/stack";
 import { Button } from "@/components/ui/button";
-import { api } from "@/convex/_generated/api";
-import { Id } from "@/convex/_generated/dataModel";
-import { useQuery } from "convex/react";
-import { useParams } from "next/navigation";
 import { useRouter } from "next/navigation";
-import { ExitIcon } from "@radix-ui/react-icons";
-import Image from "next/image";
+import {
+  ExitIcon,
+  RocketIcon,
+  TargetIcon,
+  ListBulletIcon,
+  InfoCircledIcon,
+} from "@radix-ui/react-icons";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import GoalScreen from "./goalScreen";
+import SprintScreen from "./sprintScreen";
 
 export default function Page() {
-  const { projectId } = useParams();
-
-  // Ensure projectId is a string (if it's an array or undefined, return null)
-  const validProjectId =
-    projectId && !Array.isArray(projectId)
-      ? (projectId as Id<"projects">)
-      : null;
-
-  const goals = useQuery(api.goals.getGoalsByProjectId, {
-    projectId: validProjectId as Id<"projects">,
-  });
-
   const router = useRouter();
 
   const goToHomePage = () => {
     router.push("/");
   };
 
-  // Use the useQuery hook with the "skip" option in the second argument if the projectId is invalid
-  const project = useQuery(
-    api.projects.getProjectById, // Always pass the query function
-    validProjectId ? { projectId: validProjectId } : "skip" // Pass "skip" if projectId is invalid
-  );
-
-  // Handle invalid projectId case
-  if (!validProjectId) {
-    return <div>Invalid project ID</div>;
-  }
-
-  // Loading state: project will be `undefined` while the query is in flight
-  if (project === undefined) {
-    return <div>Loading project...</div>;
-  }
-
-  // Error handling: if no project is returned, it means the project was not found
-  if (!project) {
-    return <div>Project not found</div>;
-  }
-
   return (
-    <main className="p-24 space-y-8">
+    <main className="p-8 space-y-8">
       <div className="">
         <Button
           variant={"teal"}
@@ -62,30 +30,50 @@ export default function Page() {
         >
           Back to projects <ExitIcon className="ml-2 h-4 w-4" />
         </Button>
-        <Stack direction="row">
-          <h1 className="text-4xl font-bold py-4 text-[#005A4E] dark:text-[#DBF9FF]">
-            {project.title} - Goals
-          </h1>
-        </Stack>
-
-        <p>{project.description}</p>
-        <Stack direction="row" justify="space-between" className="pt-4">
-          <p>Filter section here</p>
-          <CreateGoalButton project={project} action="create" />
-        </Stack>
-        <Stack className="py-2" direction="column" spacing={6}>
-          {goals && goals.length < 1 ? (
-            <div className="flex flex-col items-center justify-center h-screen">
-              <Image alt="plant" src="/plant.png" width={300} height={300} />
-              <p>
-                Looks like you don&apos;t have any goals yet, you could upload one by
-                clicking the button above
-              </p>
-            </div>
-          ) : (
-            goals?.map((goal) => <GoalCard key={goal._id} goal={goal} />)
-          )}
-        </Stack>
+        <Tabs defaultValue="sprint" className="py-4">
+          <TabsList className="grid w-full grid-cols-4 bg-[#FAFCFC] dark:bg-[#05201D] border border-[#05201D] dark:border-[#57E1C0] backdrop-blur-md shadow-lg dark:shadow-xl">
+            <TabsTrigger
+              value="sprint"
+              className="text-[#005A4E] dark:text-[#DBF9FF]"
+            >
+              Sprints{" "}
+              <RocketIcon className="text-[#005A4E] dark:text-[#DBF9FF] bg-inherit ml-4 h-4 w-4" />{" "}
+            </TabsTrigger>
+            <TabsTrigger
+              value="goals"
+              className="text-[#005A4E] dark:text-[#DBF9FF]"
+            >
+              Goals{" "}
+              <TargetIcon className="text-[#005A4E] dark:text-[#DBF9FF] bg-inherit ml-4 h-4 w-4" />{" "}
+            </TabsTrigger>
+            <TabsTrigger
+              value="tasks"
+              className="text-[#005A4E] dark:text-[#DBF9FF]"
+            >
+              Tasks{" "}
+              <ListBulletIcon className="text-[#005A4E] dark:text-[#DBF9FF] bg-inherit ml-4 h-4 w-4" />{" "}
+            </TabsTrigger>
+            <TabsTrigger
+              value="insights"
+              className="text-[#005A4E] dark:text-[#DBF9FF]"
+            >
+              Insights{" "}
+              <InfoCircledIcon className="text-[#005A4E] dark:text-[#DBF9FF] bg-inherit ml-4 h-4 w-4" />{" "}
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="sprint">
+            <SprintScreen />
+          </TabsContent>
+          <TabsContent value="goals">
+            <GoalScreen />
+          </TabsContent>
+          <TabsContent value="tasks">
+            <h1>tasks</h1>
+          </TabsContent>
+          <TabsContent value="insights">
+            <h1>Insights</h1>
+          </TabsContent>
+        </Tabs>
       </div>
     </main>
   );
