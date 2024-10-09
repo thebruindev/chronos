e8c5b71750b80a80722f18f62f813fee60de96f8
thebruindev <yoanmaudon@gmail.com>
2024-10-09 12:33:54 +0200

    working on crud actions for tasks on the frontend

--- app/tasks/taskCard.tsx
+++ app/tasks/taskCard.tsx
@@ -1,29 +1,34 @@
 import Stack from "@/components/stack";
-// import { Doc } from "@/convex/_generated/dataModel";
-// import { UpdateGoalButton } from "./update-goal-button";
-// import { DeleteGoalButton } from "./delete-goal-button";
-import Chip, { ResultValuesMap } from "@/components/chip";
-import {
-  CheckCircledIcon,
-  UpdateIcon,
-  CircleIcon,
-} from "@radix-ui/react-icons";
+import { PriorityValuesMap } from "@/components/chip";
+
 import { TaskWithGoal } from "@/convex/tasks";
+import { progressStatusIcon, progressStatusValuesMap } from "@/utils/cardIcons";
 
-export type progressStatusValuesMap = "Completed" | "In Progress" | "To Do";
+import { ReactNode } from "react";
+import {
+  ChevronsDown,
+  ChevronsUp,
+  EllipsisVertical,
+  Equal,
+} from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { EyeOpenIcon } from "@radix-ui/react-icons";
+import { DeleteTaskButton } from "./delete-task-button";
+import { UpdateTaskButton } from "./update-task-button";
 
-export const progressStatusIcon: Record<
-  progressStatusValuesMap,
-  React.ReactNode
-> = {
-  Completed: (
-    <CheckCircledIcon className="text-[#11C99D] bg-inherit ml-4 h-4 w-4 font-bold" />
+const taskPriorityIcon: Record<PriorityValuesMap, ReactNode> = {
+  High: (
+    <ChevronsUp className="text-[#FB7185] bg-inherit ml-4 h-4 w-4 font-bold" />
   ),
-  "In Progress": (
-    <UpdateIcon className="text-[#FB923C] bg-inherit ml-4 h-4 w-4 font-bold" />
-  ),
-  "To Do": (
-    <CircleIcon className="text-[#FB7185] bg-inherit ml-4 h-4 w-4 font-bold" />
+
+  Medium: <Equal className="ml-4 h-4 w-4 font-bold" />,
+
+  Low: (
+    <ChevronsDown className="text-[#22D3EE] bg-inherit ml-4 h-4 w-4 font-bold" />
   ),
 };
 
@@ -44,22 +49,43 @@ export function TaskCard({
             <Stack direction="row" className="mx-4" align="center">
               <p className="w-1/3">{task.issueKey}</p>
               <p className="w-1/3">{task.title}</p>
+
+              <p className="w-1/3">{task.goal?.title}</p>
               <p className="w-2/12 flex items-center">
                 {task.status}{" "}
                 {progressStatusIcon[task.status as progressStatusValuesMap]}{" "}
               </p>
-              <Chip
-                label={task.priority as ResultValuesMap}
-                variant={"outlined"}
-                width="w-1/12"
-              />
-              <p className="w-1/3">{task.goal?.title}</p>
 
               <Stack direction="row" className="w-2/12" justify="flex-end">
-                {" "}
-                {/* Set width and alignment for buttons */}
-                {/* <UpdateGoalButton goal={goal} action="update" />
-              <DeleteGoalButton goal={goal} /> */}
+                <div>
+                  {taskPriorityIcon[task.priority as PriorityValuesMap]}
+                </div>
+
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <EllipsisVertical className="ml-4 h-4 w-4 font-bold hover: cursor-pointer" />
+                  </PopoverTrigger>
+                  <PopoverContent className="bg-[#FAFCFC] dark:bg-[#05201D] w-full">
+                    <div>
+                      <Stack
+                        direction="row"
+                        align="center"
+                        className="py-2 px-2 hover:bg-[rgba(241,255,253,0.86)] dark:hover:bg-[rgba(17,43,37,0.6)] hover:shadow-xl transition-all duration-300 ease-in-out rounded text-[#005A4E] dark:text-[#DBF9FF] hover: cursor-pointer"
+                      >
+                        <p>Peek</p>
+                        <EyeOpenIcon className="h-4 w-4" />
+                      </Stack>
+                      <div>
+                        {" "}
+                        <UpdateTaskButton task={task} action="update" />
+                      </div>
+                      <div>
+                        {" "}
+                        <DeleteTaskButton task={task} />
+                      </div>
+                    </div>
+                  </PopoverContent>
+                </Popover>
               </Stack>
             </Stack>
           </div>
